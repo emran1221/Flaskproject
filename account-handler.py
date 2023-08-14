e6066ec7d5efd1b7d2508c44d147620434bba69f
abdullahqalawi <abdllahqalawi@gmail.com>
2023-08-14 21:05:08 +0300

    i destroy it

--- account-handler.py
+++ account-handler.py
@@ -1,19 +1,19 @@
 import math
 
 def calculate_circle_area(radius):
-    return math.pi * radius ** 2
+    return math.pi * radius * 2
 
 def main():
-    print("Welcome to the Circle Area Calculator!")
+    print("Welcome  the  Area Calculator!")
     try:
-        radius = float(input("Enter the radius of the circle: "))
+        radius = float(input("Enter the radius of  : "))
         if radius < 0:
-            print("Radius cannot be negative.")
+            print(" cannot be negative.")
         else:
             area = calculate_circle_area(radius)
-            print(f"The area of the circle with radius {radius} is {area:.2f}")
+            print(f"The area of  circle  radius {radius} is {area:.2f}")
     except ValueError:
-        print("Invalid input. Plllease enter a valid number.")
+        print("Invalid input. Plllease enter a valid .")
 
 if __name__ == "__main__":
     main()
